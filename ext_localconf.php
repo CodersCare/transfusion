d10091befa0e2ece537ccf50126f6bb9be31b673
Jo Hasenau <info@cybercraft.de>
2024-01-14 18:25:13 +0100

    [FEATURE] Add disconnect button to connected and mixed language columns

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,4 @@
 <?php
-defined('TYPO3') or die('Access denied.');
+defined('TYPO3') or die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:transfusion/Configuration/TsConfig/Page/All.tsconfig">');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:transfusion/Configuration/TsConfig/All.tsconfig">');
